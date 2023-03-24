6b0f0038e659ed55126b11c299e41ef47d49eedb
sk3316 <sk3316@srmist.edu.in>
2023-03-24 22:18:02 +0530

    first commt

--- jalwa.js
+++ jalwa.js
@@ -13,3 +13,18 @@
       document.getElementById(tabIndex).style.display = "block";
    }
 // </script>
+
+function search_animal() {
+   let input = document.getElementById('searchbar').value
+   input=input.toLowerCase();
+   let x = document.getElementsByClassName('animals');
+     
+   for (i = 0; i < x.length; i++) { 
+       if (!x[i].innerHTML.toLowerCase().includes(input)) {
+           x[i].style.display="none";
+       }
+       else {
+           x[i].style.display="list-item";                 
+       }
+   }
+}
